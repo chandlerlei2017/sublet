1c56c25856fd669728bae30ce86bb894d0500950
chandlerlei2017 <chandlerlei87@gmail.com>
2019-05-29 17:14:11 -0400

    add listing form

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,33 @@
 class ListingsController < ApplicationController
   before_action :authenticate_user!
-  def new; end
-  def create; end
+  def new
+  end
 
+  def create
+    listing = current_user.listings.new()
+    listing.assign_attributes(listing_params)
+    byebug;
+    if listing.save
+      flash[:notice] = "Your listing was successfully created"
+    else
+      flash[:notice] = "Sorry! Your listing could not be created!"
+    end
+    redirect_to user_root_path
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(
+      :name,
+      :street,
+      :city,
+      :postal_code,
+      :country,
+      :uw_dist,
+      :laurier_dist,
+      :ammenities,
+      :description
+    )
+  end
 end
