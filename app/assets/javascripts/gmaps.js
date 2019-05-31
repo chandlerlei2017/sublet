4cd9921ad851e8a6d3438e5ed12aeefed76ca285
chandlerlei2017 <chandlerlei87@gmail.com>
2019-05-30 20:46:33 -0400

    google maps api

---
+++ app/assets/javascripts/gmaps.js
@@ -0,0 +1,18 @@
+function showMap(address) {
+  var geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      var newMap = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14
+      });
+      newMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: newMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
