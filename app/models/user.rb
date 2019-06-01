54bcfc770a3a862933084baa423d5a83745e3dd9
chandlerlei2017 <chandlerlei87@gmail.com>
2019-06-01 17:47:26 -0400

    update listings viewing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :listings
-  has_one :saved_listing
-  has_one :applied_listing
+  has_many :listings, dependent: :destroy
+  has_one :saved_listing, dependent: :destroy
+  has_one :applied_listing, dependent: :destroy
 
   has_many :comments, primary_key: 'email', foreign_key: 'email'
   has_many :replies, primary_key: 'email', foreign_key: 'email'
