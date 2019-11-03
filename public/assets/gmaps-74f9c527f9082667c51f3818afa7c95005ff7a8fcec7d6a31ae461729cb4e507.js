ad8762c93bfcbe2847cab9f9841aba7fe243d3b3
chandlerlei2017 <chandlerlei87@gmail.com>
2019-11-03 12:14:27 -0500

    assets

---
+++ public/assets/gmaps-74f9c527f9082667c51f3818afa7c95005ff7a8fcec7d6a31ae461729cb4e507.js
@@ -0,0 +1,19 @@
+function showMap(address, element) {
+  var geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      var newMap = new google.maps.Map(document.getElementById(element), {
+        zoom: 15
+      });
+      newMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: newMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+;
